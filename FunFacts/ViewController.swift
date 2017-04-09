d63bcb14733278e4143b232809ebb37a88b61b4b
Sarah Kohl <sarahcharlottekohl@gmail.com>
2017-04-08 20:06:34 -0700

    Added color randomizer and icons, tested successfully on iPhone 6s

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
     let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,9 +29,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showFact() {
-        
-        
         funFactLabel.text = factProvider.randomFact()
-        
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
     }
 }
